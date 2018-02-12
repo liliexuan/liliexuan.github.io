4b5c727174ffeb398e1038117211b75a798a63f2
liliexuan <liliexuan@163.com>
2018-02-12 18:16:06 +0800

    文字改动

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fireworks">
-    <h1>{{msg}}</h1>
+    <p><a href="#">{{msg}}</a></p>
   </div>
 </template>
 
@@ -11,7 +11,7 @@ export default {
   name: 'index',
   data () {
     return {
-      msg: '老婆爱你么么哒哟！'
+      msg: '老婆,爱你么么哒哟！'
     }
   },
   methods: {
@@ -25,6 +25,25 @@ export default {
       width: '100%',
       height: '100%'
     })
+    $.fn.typewriter = function() {
+      this.each(function() {
+        var $ele = $(this), str = $ele.html(), progress = 0
+        $ele.html('')
+        var timer = setInterval(function () {
+          var current = str.substr(progress, 1)
+          if (current === '<') {
+            progress = str.indexOf('>', progress) + 1
+          } else {
+            progress++
+          }
+          $ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''))
+          if (progress >= str.length) {
+            clearInterval(timer)
+          }
+        }, 75)
+      })
+      return this
+    }
   }
 }
 </script>
@@ -44,5 +63,61 @@ li {
 }
 a {
   color: #42b983;
+}
+a:hover{
+color:#ffffff;  
+}
+
+@font-face {
+  font-family: 'Audiowide';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Audiowide'), local('Audiowide-Regular'), url(http://themes.googleusercontent.com/static/fonts/audiowide/v2/8XtYtNKEyyZh481XVWfVOj8E0i7KZn-EPnyo3HZu7kw.woff) format('woff');
+}
+
+a{
+  color:#FF9900;
+  font-family:Audiowide;
+}
+a:hover{
+  -webkit-animation: neon5 1.5s ease-in-out infinite alternate;
+  -moz-animation: neon5 1.5s ease-in-out infinite alternate;
+  animation: neon5 1.5s ease-in-out infinite alternate; 
+}
+
+p{
+  text-align:center;
+  font-size:7em;
+  margin:20px 0 20px 0; 
+}
+
+a{
+  text-decoration:none; 
+  -webkit-transition: all 0.5s;
+  -moz-transition: all 0.5s;
+  transition: all 0.5s;
+}
+
+@-moz-keyframes neon5 {
+  from {
+    text-shadow: 0 0 10px #fff,
+               0 0 20px  #fff,
+               0 0 30px  #fff,
+               0 0 40px  #FF9900,
+               0 0 70px  #FF9900,
+               0 0 80px  #FF9900,
+               0 0 100px #FF9900,
+               0 0 150px #FF9900;
+  }
+  to {
+    text-shadow: 0 0 5px #fff,
+               0 0 10px #fff,
+               0 0 15px #fff,
+               0 0 20px #FF9900,
+               0 0 35px #FF9900,
+               0 0 40px #FF9900,
+               0 0 50px #FF9900,
+               0 0 75px #FF9900;
+  }
 }
 </style>
